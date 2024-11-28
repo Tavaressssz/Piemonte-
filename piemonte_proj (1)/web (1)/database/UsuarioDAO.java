4267841b9810b7733403f703e941e7ffe0e26b5d
Tavaressssz <kauat531@gmail.com>
2024-11-28 16:50:40 -0300

    UsuarioDAO.java

--- piemonte_proj (1)/web (1)/database/UsuarioDAO.java
+++ piemonte_proj (1)/web (1)/database/UsuarioDAO.java
@@ -25,37 +25,98 @@ public class UsuarioDAO {
        
        ResultSet res = prep.executeQuery();
        
-       while( res.next() ) {
-           Usuario user = new Usuario();
-           
-           user.setId( res.getInt("id") );
-           user.setNome(res.getString("nome") );
-           user.setEmail(res.getString("email") );
-           user.setNascimento(res.getString("nascimento") );
-           user.setSenha(res.getString("senha") );
-           user.setNoticias( res.getInt("noticias") == 1 );
-           
-           list.add(user);
-       }
+     
        
-       prep.close();
+       //Continua...
+       
+       while(res.next() ) {
+           Usuario u = new Usuario();
+           u.setId(res.getInt("id"));
+           u.setNome(res.getString("nome"));
+           u.setEmail(res.getNString("email"));
+           u.setNascimento(res.getString("nascimento"));
+           u.setSenha(res.getString("senha"));
+           u.setNoticias(res.getInt("noticias")== 1);
+           
+           list.add(u);
+           
+       }
+               
+        prep.close();        
        return list;
     }
     
+    public void setNewUser ( Usuario user)throws SQLException{
+    String query = "insert into usuarios(nome,email,nascimento,senha,noticias )  "
+            + "values (?,?,?,?,?) "  ;
     
-    public void setNewUser(Usuario user) throws SQLException {
-        String query = "insert into usuarios(nome, email, nascimento, senha, noticias) "
-                     + "values(?, ?, ?, sha1(?), ?)";
-        
-        PreparedStatement prep = conn.prepareStatement(query);
-        
-        prep.setString(1, user.getNome() );
-        prep.setString(2, user.getEmail() );
-        prep.setString(3, user.getNascimento() );
-        prep.setString(4, user.getSenha() );
-        prep.setInt(5, user.isNoticias() ? 1 : 0 );
-        
-        prep.execute();
-        prep.close();
+      PreparedStatement prep = conn.prepareStatement(query);
+    
+      
+      prep.setString(1, user.getNome() );
+      prep.setString(2, user.getEmail() );
+      prep.setString(3, user.getNascimento() );
+      prep.setString(4, user.getSenha() );
+      prep.setInt(5, (user.isNoticias()) ? 1:0);
+      
+      prep.execute();
+      prep.close();
+       
+      
     }
+    
+    public void deleteUser(int id){
+        String query = "delete from usuarios where id = "+id;
+    }
+    
+    public void updateUser ( Usuario user)throws SQLException{
+    String query = "update usuarios set "
+                 + "nome = ?, email = ?, "
+                 + "nacimento = ?, noticias = ? "
+                 + "where id = ?";
+    
+      PreparedStatement prep = conn.prepareStatement(query);
+    
+      
+      prep.setString(1, user.getNome() );
+      prep.setString(2, user.getEmail() );
+      prep.setString(3, user.getNascimento() );
+      prep.setInt(4, (user.isNoticias()) ? 1:0);
+      prep.setInt(5, user.getId() );
+      
+      prep.execute();
+      prep.close();
+       
+      
+    }
+      public Usuario getOneUser(int id) throws SQLException {
+        
+       
+       String query = "select * from usuarios where id = " + id;
+       
+       PreparedStatement prep = conn.prepareStatement(query);
+       
+       ResultSet res = prep.executeQuery();
+       
+     
+       
+       //Continua...
+       Usuario u = new Usuario();  
+       
+       if(res.next() ) {                
+           u.setId(res.getInt("id"));
+           u.setNome(res.getString("nome"));
+           u.setEmail(res.getNString("email"));
+           u.setNascimento(res.getString("nascimento"));
+           u.setSenha(res.getString("senha"));
+           u.setNoticias(res.getInt("noticias")== 1);
+           
+      
+           
+       }
+               
+        prep.close();        
+       return u;
+    }
+        
 }
